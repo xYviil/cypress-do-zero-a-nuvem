c61e26e93225174ddcc0ddaa75e2dc5b50296ccc
xYviil <viniciusmeurer2012@gmail.com>
2025-07-20 16:25:10 -0300

    Exercício extra 1

--- cypress/e2e/CAT-TAT.cy.js
+++ cypress/e2e/CAT-TAT.cy.js
@@ -1,6 +1,22 @@
 describe('Central de Atendimento ao Cliente TAT', () => {
+    beforeEach (() => {
+        cy.visit('./src/index.html')
+    } )
+
   it('verifica o título da aplicação', () => {
-    cy.visit('./src/index.html')
     cy.title('Central de Atendimento ao Cliente TAT').should('be.equal', 'Central de Atendimento ao Cliente TAT')
+  })
+
+  it('Preenche os campos obrigatórios e envia ao formulário', () => {
+
+    const longTest = Cypress._.repeat('abcdefghijklmnopqrstuvwxyz', 10)
+
+    cy.get('#firstName').type('Vinícius')
+    cy.get('#lastName').type('Meurer Petry')
+    cy.get('#email').type('viniciusmeurer2012@gmail.com')
+    cy.get('#open-text-area').type(longTest, {delay: 0 })
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.success').should('be.visible')
   })
 })
